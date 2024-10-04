8da4bf52f70684152b9e09975e7e801a0a4e7bc5
Higeners <deminmikl@gmail.com>
2024-10-04 17:47:53 +0300

    xrandr

--- lib/xdrives.cpp
+++ lib/xdrives.cpp
@@ -1,12 +1,17 @@
 #include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
     Display* display = XOpenDisplay(NULL);
-    Screen* screen = XScreenOfDisplay(display, 0);
-    std::cout << XWidthOfScreen(screen) << 'x' << XHeightOfScreen(screen) << '\n';
-
+    int screen_count = XScreenCount(display);
+    int min_width, min_height, max_width, max_height;
+    XRRGetScreenSizeRange(display, NULL, &min_width, &min_height, &max_width, &max_height);
+    std::cout << min_width << 'x' << min_height << '\n';
+    std::cout << max_width << 'x' << max_height << '\n';
+    
+    
 
     return 0;
 }
