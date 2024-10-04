01cddbbf9f622eeca3edc3972ceafabf57943cee
Higeners <deminmikl@gmail.com>
2024-10-04 16:51:51 +0300

    Display

--- lib/xdrives.cpp
+++ lib/xdrives.cpp
@@ -1,8 +1,12 @@
 #include <X11/Xlib.h>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    
+    Display* display = XOpenDisplay(NULL);
+    Screen* screen = XScreenOfDisplay(display, 0);
+    std::cout << XWidthOfScreen(screen) << 'x' << XHeightOfScreen(screen) << '\n';
+
 
     return 0;
 }
