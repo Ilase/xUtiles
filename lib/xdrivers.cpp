00bff003ad52d32b3109ee222a91a4468104d2b1
Ilase <igeodec@gmail.com>
2024-10-07 15:06:07 +0300

    backup preparser

--- lib/xdrivers.cpp
+++ lib/xdrivers.cpp
@@ -13,14 +13,48 @@ std::pair<int, int> xdr::getResolution() {
     return std::pair<int, int>(XDisplayWidth(display,0), XDisplayHeight(display,0));
 }
 
-void xdr::backup(){
+bool xdr::check_existing(const fs::path &p, fs::file_status s)
+{
+    if(fs::status_known(s) ? fs::exists(s) : fs::exists(p)){
+        return true;
+    }
+    return false;
+}
 
-    fs::path conf_path_x11 = "/etc/x11/";
+int xdr::backup(){
+
+    fs::path conf_path_x11 = "/etc/X11/";
     fs::path conf_path_modprobe_dir = "/etc/modprobe.d";
+    fs::path conf_backup_dir = "/etc/xdrives/backups";
     //
     std::cout << "Backuping default path config" << std::endl;
     std::cout << "Current config path for backup:\n" 
         << "x11 : " << conf_path_x11 << "\n"  
-        << "modeprobe_dir : " << conf_path_modprobe_dir << std::endl; 
+        << "modeprobe : " << conf_path_modprobe_dir << std::endl; 
     //
+    if(!check_existing(conf_path_x11)){
+        std::cout << conf_path_x11 << " does not exists!" << std::endl;
+        return XDR_ERR;
+    }
+    if(!check_existing(conf_path_modprobe_dir)){
+        std::cout << conf_path_modprobe_dir << " does not exists!" << std::endl;
+        return XDR_ERR;
+    }
+    
+    //
+    char ans;
+    std::cout << "Process the backuping configuration? : ";
+    std::cin >> ans;
+    //
+    switch(ans){
+        case 'y':
+        break;
+        case 'n':
+        break;
+        default: 
+            std::cerr << "Unknown char!";
+            return XDR_ERR;
+            break;
+    } 
+    return 0;
 }
--- lib/xdrivers.hpp
+++ lib/xdrivers.hpp
@@ -1,26 +1,32 @@
-#if !defined(XDRIVERS_HPP)
-#define XDRIVERS_HPP
+#ifndef XDR
 //
-#include <iostream>
-#include <stdexcept>
-#include <utility>
-#include <algorithm>
-#include <filesystem>
+#define XDR
 //
-namespace fs = std::filesystem;
+#define XDR_ERR 11
+#define XDR_OK 10
 //
-#include <X11/Xlib.h>
-#include <GL/glx.h>
-//
-namespace xdr
-{
-    void xdr_handler(const std::exception& e, const std::string& add_info);
-    std::pair<int, int> getResolution();
-
-    void backup();
-    void backup(char *args[]);
-}
-
-#endif // XDRIVERS_HPP
-
-
+    #if !defined(XDRIVERS_HPP)
+    #define XDRIVERS_HPP
+    //
+    #include <iostream>
+    #include <stdexcept>
+    #include <utility>
+    #include <algorithm>
+    #include <filesystem>
+    #include <string_view>
+    //
+    namespace fs = std::filesystem;
+    //
+    #include <X11/Xlib.h>
+    #include <GL/glx.h>
+    //
+    namespace xdr
+    {
+        void xdr_handler(const std::exception& e, const std::string& add_info);
+        std::pair<int, int> getResolution();
+        bool check_existing(const fs::path& p, fs::file_status s = fs::file_status{});
+        int backup();
+        int backup(char *args[]);
+    }
+    #endif // XDRIVERS_HPP
+#endif // XDR
