b7b3450ec2b485209bfffb98d72953b85b570e33
Higeners <deminmikl@gmail.com>
2024-10-07 10:41:11 +0300

    list all resolutions using xf86

--- lib/xdrives.cpp
+++ lib/xdrives.cpp
@@ -1,17 +1,89 @@
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
+#include <X11/extensions/xf86vmode.h>
 #include <iostream>
+#include <stdlib.h>
+#include <cstdarg>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <cstdio>
+
+std::string exec(const char* cmd) {
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while (!feof(pipe.get()))
+    {
+        if (fgets(buffer, 128, pipe.get()) != NULL){
+            result += buffer;
+        }
+    }
+    return result;
+}
+
+// int FindBestVideoMode(int screen, unsigned int &width, unsigned int &height)
+// {
+//     int modeCount;
+//     XF86VidModeModeInfo** modes;
+
+//     if (XF86VidModeGetAllModeLines(display, screen, &modeCount, &modes))
+//     {
+//         for(int i = 0; i < modeCount; i ++)
+//         {
+//             std::cout << modes[i]->vdisplay << 'x' <<  modes[i]->hdisplay << '\n';
+//         }
+//         XFree(modes);
+//     }
+// }
+
+// void SwitchVideoMode(int screen, int mode)
+// {
+//     if (mode >= 0)
+//     {
+//         int modeCount;
+//         XF86VidModeModeInfo** modes;
+
+//         if (XF86VidModeGetAllModeLines(display, screen, &modeCount, &modes))
+//         {
+//             if (mode < modeCount)
+//             {
+//                 XF86VidModeSwitchToMode(display, screen, modes[mode]);
+//                 XF86VidModeSetViewPort(display, screen, 0, 0);
+
+
+//                 XFlush(display);
+//             }
+
+//             XFree(modes);
+//         }
+//     }
+// }
+
 
 int main(int argc, char const *argv[])
 {
-    Display* display = XOpenDisplay(NULL);
-    int screen_count = XScreenCount(display);
-    int min_width, min_height, max_width, max_height;
-    XRRGetScreenSizeRange(display, NULL, &min_width, &min_height, &max_width, &max_height);
-    std::cout << min_width << 'x' << min_height << '\n';
-    std::cout << max_width << 'x' << max_height << '\n';
-    
-    
+    Display* display = XOpenDisplay(":0");
+    Screen* screen = XScreenOfDisplay(display, 0);
+    // XRRScreenResources *screen = XRRGetScreenResources(display, DefaultRootWindow(display));
+    // for (size_t i = 0; i < screen->ncrtc; i++)
+    // {
+    //     XRRCrtcInfo *crtc_info = XRRGetCrtcInfo(display, screen, screen->crtcs[i]);  
+    //     std::cout << i << '\t' << crtc_info->width << 'x' << crtc_info-> height << '\n';
+    //     /* code */
+    // }
+    int modeCount;
+    XF86VidModeModeInfo** modes;
 
+    if (XF86VidModeGetAllModeLines(display, 0, &modeCount, &modes))
+    {
+        for(int i = 0; i < modeCount; i ++)
+        {
+            std::cout << i << '\t' << modes[i]->hdisplay << 'x' <<  modes[i]->vdisplay << '\n';
+        }
+        XFree(modes);
+    }
+    //std::cout << exec("xrandr");
     return 0;
 }
