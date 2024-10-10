41872cfd6fac88982a765a36bb3e08f9a27e6d84
Ilase <igeodec@gmail.com>
2024-10-10 10:23:07 +0300

    filed emptyies

--- lib/main.cpp
+++ lib/main.cpp
@@ -6,22 +6,24 @@ int main(int argc, char const *argv[])
     int opt;
     bool display_opt;
     bool backup_opt;
-        const char *_backup_folder_path = nullptr;
-    while(true);
+    const char *_backup_folder_path = nullptr;
+    //while(true);
     //
     xdr::xDriver app = xdr::xDriver();
     //
-    switch (argv[1][0]){
-        case 'b':
-            std::cout << "Option 'backup' selected." << std::endl;
-            break;
-        case 'd':
-            std::cout << "Option 'display' selected" << std::endl;
-            break;
-        default:
-            std::cerr << "Unknown option: " << argv[1] << std::endl;
-            return XDR_ERR;
-            break;
+    if(argv[1] != NULL){
+        switch (argv[1][1]){ // catch the line
+            case 'b':
+                std::cout << "Option 'backup' selected." << std::endl;
+                break;
+            case 'd':
+                std::cout << "Option 'display' selected" << std::endl;
+                break;
+            default:
+                std::cerr << "Unknown option: " << argv[1] << std::endl;
+                return XDR_ERR;
+                break;
+        }
     }
     return XDR_OK;
 }
--- lib/xdrivers.hpp
+++ lib/xdrivers.hpp
@@ -1,60 +1,81 @@
 #ifndef XDR
 //
-#define XDR                                         //LIBRARY SPACE
+#define XDR // LIBRARY SPACE
 ////
 //--------------------------------------------------//------------------------------
-#define XDR_ERR 11                                  //status codes
-#define XDR_OK 10                                   //
-//--------------------------------------------------//------------------------------
-    #if !defined(XDRIVERS_HPP)                      //
-    #define XDRIVERS_HPP                            //
-    //----------------------------------------------//----------------------------------
-    #include <iostream>                             //basic input/output stream
-    #include <stdexcept>                            //exception class for handler
-    #include <utility>                              //std containers
-    #include <algorithm>                            //std functions
-    #include <filesystem>                           //lib for working with filesystem
-    #include <string_view>                          //view strings in fs iteration 
-    #include <vector>                               // paths container 
-    #include <chrono>                               //
-    #include <ctime>                                //
-    #include <iomanip>
-    //----------------------------------------------//----------------------------------
-    namespace fs = std::filesystem;                 //shortcut for std::filesystem
-    //
-    #include <X11/Xlib.h>
-    #include <GL/glx.h>
-    //
-    namespace xdr
-    {   
-        void xdr_handler(const std::exception& e, const std::string& add_info);
-        /*
-        @brief Main app class whos make operations woth backups
-        */
-        class xDriver {
-            private:
-                fs::path backup_path = "/mnt/xdr-backups/";
-                fs::path X11_d = "/etc/X11/";
-                fs::path MDP_d = "/etc/modprobe.d/";
-                std::vector<fs::path> backup_list;
-            public:
-                xDriver(fs::path def_p = fs::path{});
-                // ~xDriver();
-                //
-                void parse_backup_list();
-                int make_backup();
-                //
-                fs::path get_backup_path();
-                fs::path get_X11_d();
-                fs::path get_MDP_d();
-        };
+#define XDR_ERR 11         // status codes
+#define XDR_OK 10          //
+                           //--------------------------------------------------//------------------------------
+#if !defined(XDRIVERS_HPP) //
+#define XDRIVERS_HPP       //
+//----------------------------------------------//----------------------------------
+#include <iostream>    //basic input/output stream
+#include <stdexcept>   //exception class for handler
+#include <utility>     //std containers
+#include <algorithm>   //std functions
+#include <filesystem>  //lib for working with filesystem
+#include <string_view> //view strings in fs iteration
+#include <vector>      // paths container
+#include <chrono>      //
+#include <ctime>       //
+#include <iomanip>
+//----------------------------------------------//----------------------------------
+namespace fs = std::filesystem; // shortcut for std::filesystem
+//
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+//
+namespace xdr
+{
+    void xdr_handler(const std::exception &e, const std::string &add_info);
+    /*
+    @brief Main app class whos make operations woth backups
+    */
+    class xDisplay
+    {
+    private:
+    public:
+        Display *display;
+        Window root;
+        int screenCount;
+        Screen *defaultScreen;
+        XRRScreenConfiguration *screenConfig;
+        XRRScreenResources *screenResources;
+        std::vector<std::vector<XRRScreenSize>> screenSizes;
+        xDisplay();
+        ~xDisplay();
+        void ChangeResolution(XRRScreenSize *screenSize);
+        void SyncChanges();
+    };
+    class xDriver
+    {
+    private:
+        fs::path backup_path = "/mnt/xdr-backups/";
+        fs::path X11_d = "/etc/X11/";
+        fs::path MDP_d = "/etc/modprobe.d/";
+        std::vector<fs::path> backup_list;
 
-        std::pair<int, int> getResolution();
-        bool check_existing(const fs::path& p, fs::file_status s = fs::file_status{});
-        //int repair_backup(fs::path &p);
-        int make_backup(fs::path &ep, fs::path &x11_path, fs::path &mod_path);
-        std::vector<fs::path> get_backup_list(fs::path &dp);
-        int repair_backup(fs::path &bp);
-    }
-    #endif // XDRIVERS_HPP
+    public:
+        xDriver(fs::path def_p = fs::path{});
+        ~xDriver();
+        //
+        void parse_backup_list();
+        int make_backup();
+        //
+        fs::path get_backup_path();
+        fs::path get_X11_d();
+        fs::path get_MDP_d();
+    };
+    void ChangeResolution(XRRScreenSize *screenSize);
+    void SyncChanges();
+    std::string GetGraphicDeviceName();
+    void ChangeResolution(int, int, std::string &);
+    std::pair<int, int> getResolution();
+    bool check_existing(const fs::path &p, fs::file_status s = fs::file_status{});
+    // int repair_backup(fs::path &p);
+    int make_backup(fs::path &ep, fs::path &x11_path, fs::path &mod_path);
+    std::vector<fs::path> get_backup_list(fs::path &dp);
+    int repair_backup(fs::path &bp);
+}
+#endif // XDRIVERS_HPP
 #endif // XDR
