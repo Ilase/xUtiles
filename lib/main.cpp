744d7e469e2d3593783124fbbbf8ba38577a9d39
Ilase <igeodec@gmail.com>
2024-10-10 12:24:35 +0300

    remaking with iterator/map arguments

--- lib/main.cpp
+++ lib/main.cpp
@@ -7,23 +7,51 @@ int main(int argc, char const *argv[])
     bool display_opt;
     bool backup_opt;
     const char *_backup_folder_path = nullptr;
-    //while(true);
+    std::vector<std::string> arguments(argv + 1, argv + argc); 
     //
-    xdr::xDriver app = xdr::xDriver();
+    xdr::xDriver app;
     //
-    if(argv[1] != NULL){
-        switch (argv[1][1]){ // catch the line
-            case 'b':
-                std::cout << "Option 'backup' selected." << std::endl;
-                break;
-            case 'd':
-                std::cout << "Option 'display' selected" << std::endl;
-                break;
-            default:
-                std::cerr << "Unknown option: " << argv[1] << std::endl;
-                return XDR_ERR;
-                break;
+    std::map<std::string, std::function<void()>> comand_map = {
+        {
+            "--help", 
+            [](){
+                std::cout << XDR_PREF << "XDrive!"<< std::endl;
+                std::cout << XDR_PREF << "Arguments: " << std::endl;
+                std::cout << XDR_PREF << "--version \t -> show utile version" << std::endl;
+                std::cout << XDR_PREF << "--backup  \t -> start backup process" << std::endl;
+            }
+        },
+        {
+            "--version",
+            [](){
+                std::cout << XDR_PREF << "Version: 1.0" << std::endl;
+            }
+        },
+        {
+            "--backup",
+            [](){
+                std::cout << XDR_PREF << "Start backup process" << std::endl;
+            }
+        },
+        {
+            "--backup-path",
+            [](/*const auto& _app*/){
+                // std::cout << XDR_PREF << app.get_backup_path() << std::endl;
+            }
         }
-    }
+
+    };
+
+
+    for(const auto &arg : arguments){
+        auto it = comand_map.find(arg);
+        if (it !=comand_map.end()){
+            it->second();
+        }else{
+            std::cout << XDR_PREF << "Unknown argument: " << arg << std::endl; 
+        }
+    } 
+
+
     return XDR_OK;
 }
--- lib/xdrivers.hpp
+++ lib/xdrivers.hpp
@@ -1,6 +1,7 @@
 #ifndef XDR
 //
 #define XDR // LIBRARY SPACE
+#define XDR_PREF "[XDR] "
 ////
 //--------------------------------------------------//------------------------------
 #define XDR_ERR 11         // status codes
@@ -9,28 +10,29 @@
 #if !defined(XDRIVERS_HPP) //
 #define XDRIVERS_HPP       //
 //----------------------------------------------//----------------------------------
-#include <iostream>    //basic input/output stream
-#include <stdexcept>   //exception class for handler
-#include <utility>     //std containers
-#include <algorithm>   //std functions
-#include <filesystem>  //lib for working with filesystem
-#include <string_view> //view strings in fs iteration
-#include <vector>      // paths container
-#include <chrono>      //
-#include <ctime>       //
-#include <iomanip>
+#include <iostream>        // basic input/output stream
+#include <stdexcept>       // exception class for handler
+#include <utility>         // std containers
+#include <algorithm>       // std functions
+#include <filesystem>      // lib for working with filesystem
+#include <string_view>     // view strings in fs iteration
+#include <vector>          // paths container
+#include <chrono>          //
+#include <ctime>           //
+#include <iomanip>         //
+#include <map>             // for argiments in main() 
+#include <functional>      // for arguments in main()
 //----------------------------------------------//----------------------------------
 namespace fs = std::filesystem; // shortcut for std::filesystem
 //
 #include <X11/Xlib.h>
-#include <X11/extensions/Xrandr.h>
+// #include <X11/extensions/Xrandr.h>
 //
 namespace xdr
 {
     void xdr_handler(const std::exception &e, const std::string &add_info);
-    /*
-    @brief Main app class whos make operations woth backups
-    */
+    #ifdef _XRANDR_H
+
     class xDisplay
     {
     private:
@@ -47,6 +49,11 @@ namespace xdr
         void ChangeResolution(XRRScreenSize *screenSize);
         void SyncChanges();
     };
+    void ChangeResolution(XRRScreenSize *screenSize);
+    #endif
+    /*
+    @brief Main app class whos make operations woth backups
+    */
     class xDriver
     {
     private:
@@ -66,7 +73,6 @@ namespace xdr
         fs::path get_X11_d();
         fs::path get_MDP_d();
     };
-    void ChangeResolution(XRRScreenSize *screenSize);
     void SyncChanges();
     std::string GetGraphicDeviceName();
     void ChangeResolution(int, int, std::string &);
