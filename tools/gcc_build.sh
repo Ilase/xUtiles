f24c29feff2e5f7fa6405a6d84f56bcafd347ea2
Ilase <igeodec@gmail.com>
2024-10-18 10:26:05 +0300

    refact for qt merge

--- tools/gcc_build.sh
+++ tools/gcc_build.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 
-SOURCE="src/xutiles-backup.cpp 
-        src/xutiles-drivers.cpp
-        src/xutiles-main-bin.cpp
-        src/xutiles.cpp"
-INCLUDE="include"
+SOURCE="src/lib/xutiles-backup.cpp 
+        src/lib/xutiles-drivers.cpp
+        src/lib/xutiles-main-bin.cpp
+        src/lib/xutiles.cpp"
+INCLUDE="include/lib/"
 
 g++ $SOURCE -I$INCLUDE -lX11 -L/usr/lib/X11 -o build/xdrive-bimba
