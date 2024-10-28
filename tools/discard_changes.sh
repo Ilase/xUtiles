d2790ade9d8d5c99605ea1c3a61431df76de3646
Ilase <igeodec@gmail.com>
2024-10-28 17:31:19 +0300

    fool

--- tools/discard_changes.sh
+++
@@ -1,4 +0,0 @@
-#/!bin/bash
-
-rm -rf /etc/X11/xorg.conf
-rm -rf /etc/X11/xorg.conf.d
---
+++ tools/xorg_changes/discard_changes.sh
@@ -0,0 +1,4 @@
+#/!bin/bash
+
+mv  /etc/X11/xorg.conf /etc/X11/xorg.conf.fool
+mv  /etc/X11/xorg.conf.d /etc/X11/xorg.conf.d.fool
