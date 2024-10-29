b767af512e185c9e04eb24b952c9b5650cd70bd9
Ilase <igeodec@gmail.com>
2024-10-29 14:09:28 +0300

    huita polnaya

---
+++ test/xorg-conf/xutiles-xconfig.hpp
@@ -0,0 +1,28 @@
+#include <utility>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <array>
+#include <memory>
+#include <stdexcept>
+namespace xdr{
+    enum graphic_type{
+        AMD,
+        INTEL,
+        NVIDIA
+    };
+    class xConfig{
+        graphic_type type;
+        std::vector<std::string> devices;
+        public:
+        xConfig();
+        void check_type();
+        std::vector<std::string> get_monitor_names();
+    };
+    int change_tearing(bool, std::string);
+    std::vector<std::string> get_adapt(const char*);
+
+    int apply_changes();
+};
---
+++ tools/qt_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+qmake-qt5 ../xDrive.pro && cd ../build/
+make -j4 # in /home/ilase/Projects/xUtiles/build
